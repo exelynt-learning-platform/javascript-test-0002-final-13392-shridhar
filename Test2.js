ae259f3fc4682b00bd78e722d0557c2e6162d1b5
shridharDB <shridhar.kulkarni@cogoport.com>
2026-03-14 20:26:15 +0530

    printed pattern

--- Test2.js
+++ Test2.js
@@ -1,25 +1,18 @@
-function invertedPascal(){
+function invertedPascal() {
 
     const MAX = 4;
 
-    for(let n = MAX; n >= 0; n--){
+    for (let n = MAX; n >= 0; n--) {
 
         let line = " ".repeat(MAX - n);
-
         let val = 1;
 
-        for(let k = 0; k <= n; k++){
-
-            if(k > 0){
-                val = val * (n - k + 1) / k;
-            }
-
-            line += val;
-
-            if(k < n) line += " ";
+        for (let k = 0; k <= n; k++) {
+            line += val + " ";
+            val = val * (n - k) / (k + 1);
         }
 
-        console.log(line);
+        console.log(line.trimEnd());
     }
 }
 
